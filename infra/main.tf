d1bf7cd2e844c4c10355f30b21b9014412dcb1c5
Khue Doan <mail@khuedoan.com>
2022-06-20 19:39:14 +0700

    refactor(infra): use SSH key generated from Terraform instead

--- infra/main.tf
+++ infra/main.tf
@@ -10,12 +10,22 @@ module "network" {
   compartment_id = module.base.compartment_id
 }
 
+resource "tls_private_key" "ssh" {
+  algorithm = "ED25519"
+}
+
+resource "local_file" "ssh_private_key" {
+  content         = tls_private_key.ssh.private_key_openssh
+  filename        = "${path.root}/private.pem"
+  file_permission = "0600"
+}
+
 module "instance" {
   source         = "./modules/instance"
   compartment_id = module.base.compartment_id
   display_name   = "horus"
   subnet_id      = module.network.subnet_id
-  ssh_public_key = file("~/.ssh/id_ed25519.pub")
+  ssh_public_key = tls_private_key.ssh.public_key_openssh
   shape = {
     name = "VM.Standard.A1.Flex"
     config = {
