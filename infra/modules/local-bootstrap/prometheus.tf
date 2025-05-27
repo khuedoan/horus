da007d036c69c4b2228aa4621e49867c182f4dbf
Khue Doan <mail@khuedoan.com>
2025-05-28 00:03:26 +0700

    feat: add Istio and Prometheus to new bootstrap

---
+++ infra/modules/local-bootstrap/prometheus.tf
@@ -0,0 +1,51 @@
+resource "kubectl_manifest" "prometheus" {
+  server_side_apply = true
+  yaml_body = yamlencode({
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name       = "prometheus"
+      namespace  = helm_release.argocd.namespace
+      finalizers = ["resources-finalizer.argocd.argoproj.io"]
+    }
+    spec = {
+      project = "default"
+      destination = {
+        name      = "in-cluster"
+        namespace = "monitoring"
+      }
+      syncPolicy = local.sync_policy
+      source = {
+        repoURL        = "https://prometheus-community.github.io/helm-charts"
+        chart          = "kube-prometheus-stack"
+        targetRevision = "72.0.1"
+        helm = {
+          valuesObject = {
+            valuesObject = {
+              grafana = {
+                additionalDataSources = [
+                  {
+                    name = "Loki"
+                    type = "loki"
+                    url  = "http://loki:3100"
+                  },
+                ]
+                enabled                = false
+                forceDeployDashboards  = true
+                forceDeployDatasources = true
+              }
+              prometheus = {
+                prometheusSpec = {
+                  podMonitorSelectorNilUsesHelmValues     = false
+                  probeSelectorNilUsesHelmValues          = false
+                  ruleSelectorNilUsesHelmValues           = false
+                  serviceMonitorSelectorNilUsesHelmValues = false
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
