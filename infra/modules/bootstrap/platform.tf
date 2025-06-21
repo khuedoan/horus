2d9c2009226ceba693ff1508be0b2a05e61e8d85
Khue Doan <mail@khuedoan.com>
2025-06-22 01:12:07 +0700

    feat(infra): bootstrap platform apps

---
+++ infra/modules/bootstrap/platform.tf
@@ -0,0 +1,25 @@
+resource "kubectl_manifest" "platform" {
+  server_side_apply = true
+  yaml_body = yamlencode({
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name       = "platform"
+      namespace  = helm_release.argocd.namespace
+      finalizers = ["resources-finalizer.argocd.argoproj.io"]
+    }
+    spec = {
+      project = "default" # TODO separate project
+      destination = {
+        name      = "in-cluster"
+        namespace = helm_release.argocd.namespace
+      }
+      syncPolicy = local.sync_policy
+      source = {
+        repoURL        = "oci://registry.127.0.0.1.sslip.io/platform" # TODO use registry var
+        targetRevision = var.cluster
+        path           = "."
+      }
+    }
+  })
+}
