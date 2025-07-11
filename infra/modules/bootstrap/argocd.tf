1e8a984265c40b22c840039541aa0a58eca3143d
Khue Doan <mail@khuedoan.com>
2025-07-11 14:51:00 +0700

    refactor(infra/bootstrap): separate logic for local k3d
    This makes k3d special, I might refactor this again later.
    
    Change-Id: I3d86a047e02edcf572167327179f61aed0ea46b5

--- infra/modules/bootstrap/argocd.tf
+++ infra/modules/bootstrap/argocd.tf
@@ -4,18 +4,18 @@ resource "helm_release" "argocd" {
   create_namespace = true
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "8.0.17"
+  version          = "8.1.3"
   timeout          = 60 * 10
+  max_history      = 1 # Revert with Terraform instead
 
   values = [
     yamlencode({
       global = {
         domain = "argocd.${var.cluster_domain}"
-        # TODO override to use the latest development version for first class OCI support
-        # Remove once ArgoCD 3.1.0 stable is released
         image = {
-          repository = "ghcr.io/argoproj/argo-cd/argocd"
-          tag        = "3.1.0-dc1d148a"
+          # TODO override to use the latest development version for first class OCI support
+          # Remove once ArgoCD 3.1.0 stable is released
+          tag = "v3.1.0-rc1"
         }
       }
       configs = {
@@ -24,29 +24,39 @@ resource "helm_release" "argocd" {
           "controller.diff.server.side" = true
         }
         cm = {
-          "timeout.reconciliation.jitter"         = "60s"
           "resource.ignoreResourceUpdatesEnabled" = true
           "resource.customizations.ignoreResourceUpdates.all" = yamlencode({
             jsonPointers = [
               "/status"
             ]
           })
-          "users.anonymous.enabled" = true
+          "admin.enabled" = false
+          "oidc.config" = yamlencode({
+            name         = "SSO"
+            issuer       = "https://dex.${var.cluster_domain}"
+            clientID     = "argocd"
+            clientSecret = "$oidc.dex.clientSecret"
+          })
+
         }
         rbac = {
-          "policy.default" = "role:admin"
+          # TODO remove k3d-specific logic?
+          "policy.default" = var.platform == "k3d" ? "role:admin" : "role:readonly"
         }
       }
       server = {
         ingress = {
           enabled          = true
           ingressClassName = "nginx"
-          tls              = false
+          annotations = {
+            "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
+          }
+          tls = !(var.platform == "k3d")
         }
       }
       repoServer = {
-        hostNetwork = true
-        dnsPolicy   = "ClusterFirstWithHostNet"
+        hostNetwork = var.platform == "k3d"
+        dnsPolicy   = var.platform == "k3d" ? "ClusterFirstWithHostNet" : "ClusterFirst"
       }
       dex = {
         enabled = false
